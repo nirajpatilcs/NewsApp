1072bdb5f4a2ea619223ed11b3d84989274190de
niraj <62448681+nirajpatilcs@users.noreply.github.com>
2025-02-14 13:37:37 +1300

    Feat: Initial fetch logic

--- screens/SearchScreen/useSearchScreen.ts
+++ screens/SearchScreen/useSearchScreen.ts
@@ -1,17 +1,28 @@
 import { useCallback, useState } from "react";
+import axios from "axios";
 import { debounce } from "lodash";
+import type { Article } from "@/types";
+import Constants from "expo-constants";
+
+const API_KEY = Constants?.expoConfig?.extra?.API_KEY;
+const BASE_URL = "https://newsapi.org/v2/everything";
 
 export const useSearchScreen = () => {
   const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [articles, setArticles] = useState<Array<Article>>([]);
 
   const debouncedSearch = useCallback(
     debounce(async (nextQuery) => {
       if (nextQuery.length < 2) {
         // Don't search if query is too short (less than 2 characters)
+        // Clear articles if query is empty
+        setArticles([]);
         return;
       }
 
-      // TODO: Implement search logic
+      await fetchNews(nextQuery);
     }, 200),
     [],
   );
@@ -21,9 +32,33 @@ export const useSearchScreen = () => {
     debouncedSearch(text);
   };
 
+  const fetchNews = async (searchQuery: string) => {
+    if (!searchQuery) return;
+
+    try {
+      const response = await axios.get(BASE_URL, {
+        params: {
+          q: searchQuery,
+          apiKey: API_KEY,
+          pageSize: 5,
+          language: "en",
+        },
+      });
+      setArticles(response.data.articles);
+    } catch (err) {
+      setError("Failed to fetch news");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return {
     data: {
       query,
+      articles,
+      error,
+      loading,
+      emptySearch: !loading && !error && !articles.length,
     },
     operations: { handleChangeText },
   };
---
+++ types.ts
@@ -0,0 +1,5 @@
+export type Article = {
+  title: string;
+  description: string;
+  urlToImage: string;
+};
